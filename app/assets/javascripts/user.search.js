ee55cb58665d19a3f6cb1959d3a27fd5e1a58d3b
YusukePro61 <learning630@gmail.com>
2019-10-17 21:24:21 +0900

    incremental_search

--- app/assets/javascripts/user.search.js
+++ app/assets/javascripts/user.search.js
@@ -1,7 +1,6 @@
 $(function(){
 
   function buildUser(user){
-    // console.log(user.name)
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
                   <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
@@ -10,7 +9,6 @@ $(function(){
   }
 
   function buildMember(mem_id, mem_name){
-   console.log(mem_name)
     var chatmember = `<div class='chat-group-user'>
                         <input name='group[user_ids][]' type='hidden' value=${mem_id}>
                         <p class='chat-group-user__name'>${mem_name}</p>
@@ -21,8 +19,7 @@ $(function(){
 
   $('#user-search-field').on("keyup", function(){
     var input = $('#user-search-field').val();
-    $('.chat-group-user').remove()
-    // console.log(input)
+    $('#user-search-result').empty()
 
     $.ajax({
       type: 'GET',
@@ -33,7 +30,6 @@ $(function(){
 
     .done(function(users){
       users.forEach(function(user){
-      // console.log(user)
       var user_name = buildUser(user);
 
         if (user.length !== 0)
@@ -55,17 +51,11 @@ $(function(){
     var mem_name = $(this).attr('data-user-name')
     console.log(mem_id, mem_name)
     var member = buildMember(mem_id, mem_name)
-    // console.log(user)
     $('#chat-group-users').append(member)
     $(this).parent().remove()
-    // var chatmember = this.parent
-    // $('.chat-group-form__field--right').append(this.parent)
-    // console.log(this)
-
   })
 
   $(document).on("click", '.js-remove-btn', function(){
-    // console.log("click2")
     $(this).parent().remove()
   })
 });
