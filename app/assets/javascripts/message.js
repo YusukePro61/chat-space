b1cf32f213ffd69c489a39ac96012e711dc36ced
YusukePro61 <learning630@gmail.com>
2019-10-19 19:58:54 +0900

    auto_update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,21 +1,64 @@
 $(function(){
 
+  function buildAnotherMSG(message){
+    var images = (message.image) ? `<img class="lower-message__image" src="${message.image}">` : '';
+    var userHTML = `<div class="main-display" data-id=${message.id}>
+                        <div class="main-display__username">
+                          ${message.user_name}
+                        <div class="main-display__username__date">
+                          ${message.created_at}
+                          </div>
+                        </div>
+                      <div class="main-display__comment">
+                        <p class="lower-message__content">
+                          ${message.content}
+                        </p>
+                          ${images}
+                      </div>
+                    </div>`
+    return userHTML
+  }
+
+  var reloadMessages = (function(){
+    var last_message_id = $('.main-display').last().data('id');
+    console.log(last_message_id);
+
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+
+    .done(function(messages){
+      // console.log('success')
+      // console.log(messages)
+      messages.forEach(function(message) {
+        var usermsg = buildAnotherMSG(message)
+        $('.main-content').append(usermsg)
+      })
+      $('.main-content').animate({ scrollTop: $('.main-content')[0].scrollHeight}, 'fast');
+    })
+    .fail(function(){
+      console.log('error');
+    })
+});
+
   function buildMessage(message){
     var img = (message.img) ? `<img class="lower-message__image" src="${message.img}">` : '';
-
-    var html = `<div class="main-display">
-                  <div class="main-display__username">
-                    ${message.name}
-                  <div class="main-display__username__date">
-                    ${message.date}
-                  </div>
-                  </div>
-                  <div class="main-display__comment">
-                    <p class="lower-message__content">
-                      ${message.content}
-                    </p>
-                      ${img}
-                  </div>
+    var html = `<div class="main-display" data-id='message.id'>
+                    <div class="main-display__username">
+                      ${message.name}
+                    <div class="main-display__username__date">
+                      ${message.date}
+                    </div>
+                    </div>
+                    <div class="main-display__comment">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                        ${img}
+                    </div>
                 </div>`
     return html;
   }
@@ -49,7 +92,7 @@ $(function(){
     });
 
   });
+  if (document.URL.match(/messages/)) {
+    setInterval(reloadMessages, 5000);
+  }
 });
-
-
-
